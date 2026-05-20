82655b40f952adb7f5c837a5fd5c09a132891e4b
joyakubu <joelyakubu269@gmail.com>
2026-05-21 00:41:58 +0100

    padding art

--- padArt.go
+++ padArt.go
@@ -1,11 +1,16 @@
 package main
 
+import "strings"
+
 func PadArtRows(rows []string, width int) []string {
 	sli := []string{}
 	for _, r := range rows {
 		padding := width - len(r)
 		if padding <= 0 {
 			sli = append(sli, r)
+		} else {
+			sli = append(sli, r+strings.Repeat(" ", padding))
 		}
 	}
+	return sli
 }
