4e9e11e954ef25ed06f63a5b8b5e7c7339b73487
joyakubu <joelyakubu269@gmail.com>
2026-05-20 23:08:15 +0100

    validating banner

--- validate_banner.go
+++ validate_banner.go
@@ -6,4 +6,7 @@ func ValidateBanner(banner map[rune][]string) error {
 	if banner == nil {
 		return fmt.Errorf("banner is nil")
 	}
+	if len(banner) != 95 {
+		return fmt.Errorf("error banner has %d entries", len(banner))
+	}
 }
