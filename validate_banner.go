a86537ae374e73c6bf3cd73ecd1c97d10982a946
joyakubu <joelyakubu269@gmail.com>
2026-05-20 23:33:42 +0100

    validating banner

--- validate_banner.go
+++ validate_banner.go
@@ -9,4 +9,19 @@ func ValidateBanner(banner map[rune][]string) error {
 	if len(banner) != 95 {
 		return fmt.Errorf("error banner has %d entries", len(banner))
 	}
+	// for r := rune(32); r <= 126; r++ {
+	// 	lines, ok := banner[r]
+	// 	if !ok {
+	// 		return fmt.Errorf("key is absent")
+	// 	}
+	// 	if len(lines) != 8 {
+	// 		return fmt.Errorf("value is not up to 8")
+	// 	}
+	// }
+	for k, v := range banner {
+		if k < rune(32) || k > rune(126) {
+			return fmt.Errorf("key is absent")
+		}
+	}
+	return nil
 }
