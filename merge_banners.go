2812fd934e56db9386bb5c993ed6890c18adba41
joyakubu <joelyakubu269@gmail.com>
2026-05-21 00:18:59 +0100

    merge  banners

--- merge_banners.go
+++ merge_banners.go
@@ -1,12 +1,5 @@
 package main
 
 func MergeBanners(base map[rune][]string, priority map[rune][]string) map[rune][]string {
-	m := make(map[rune][]string)
-	for k, v := range base {
-		m[k] = v
-	}
-	for k, v := range priority {
-		m[k] = v
-	}
-	return m
+
 }
